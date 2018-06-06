04e5415e94e0fdccf02d165c2382cda0374d5773
chit786 <chit786@gmail.com>
2018-06-06 15:20:28 +0200

    Second Commit

---
+++ TEmplate 1 (2).groovy
@@ -0,0 +1,273 @@
+// Exported from:        https://Chitrangs-MacBook-Pro.local:6616/xlr/#/templates/Folder990ea4a147894cf3b1c31aa4503bea0e-Release118dd0f94ad547319499aed1f3793549/releasefile
+// XL Release version:   8.0.2-SNAPSHOT
+// Date created:         Wed Jun 06 15:20:27 CEST 2018
+
+def scmConnectorConfig1 = 'PLEASE IMPLEMENT THIS METHOD'
+
+xlr {
+  template('TEmplate 1 (2)') {
+    folder('f2')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-05-23T09:00:00+0200')
+    scmConnectorConfig scmConnectorConfig1
+    phases {
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          custom('JIRA') {
+            script {
+              type 'jira.CreateIssue'
+              
+            }
+          }
+          manual('XLD') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'XLD\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.Deploy\' could not be found because of a missing plugin.'
+          }
+          manual('cmmunut\\]]]') {
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            flagComment 'Task \'cmmunut\\]]]\' in Phase \'New Phase\' has been replaced by a manual task. The task of type \'xldeploy.DeployTask\' could not be found because of a missing plugin.'
+          }
+        }
+      }
+    }
+    attachments {
+      attachment("sql:Attachment4098b6879bc7413c8c3d0e8079890ecc") {
+        filename "attachments/Attachment4098b6879bc7413c8c3d0e8079890ecc-image.png"
+      }
+    }
+    
+  }
+}
