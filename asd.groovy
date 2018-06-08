8baae7668c2e0ed7f8a239164dd6cf594c8b49cd
chit786 <chit786@gmail.com>
2018-06-08 09:50:44 +0200

    as

--- asd.groovy
+++ asd.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Chitrangs-MacBook-Pro.local:5516/#/templates/Folderd2c2f73be2ae4d5692b2353f6a8303f2-Folder700e84cbc0d24eb3bd925875ae7a3e6a-Release5cdcf5343a6a4ffcadbffe081d89b5c7/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Fri Jun 08 09:38:05 CEST 2018
+// Date created:         Fri Jun 08 09:50:43 CEST 2018
 
 def riskProfile(title) {
     riskApi.getRiskProfileByTitle(title)
@@ -32,12 +32,19 @@ xlr {
       phase('New Phase') {
         tasks {
           manual('T1') {
-            
+            precondition 'safadf'
           }
           manual('T2') {
-            
+            failureHandler 'sadsafdvasdg wrG A G AEG \n' +
+                           's\n' +
+                           ' daegea'
+            failureHandlerScriptEnabled true
+            taskRecoverOp com.xebialabs.xlrelease.domain.recover.TaskRecoverOp.FAIL_TASK
           }
           manual('T3') {
+            
+          }
+          manual('hgfdghd') {
             
           }
         }
