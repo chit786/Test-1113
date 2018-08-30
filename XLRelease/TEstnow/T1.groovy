6f78ae795340b22bf0db98bb7107e9737454c4ec
chit786 <chit786@gmail.com>
2018-08-30 14:41:39 +0200

    v1

---
+++ XLRelease/TEstnow/T1.groovy
@@ -0,0 +1,20 @@
+// Exported from:        https://Chitrangs-MBP.xebialabs.com:5516/xlr/#/templates/Folder21d097475e154858aef8d346c8736245-Releaseab377157eca847faa57b448437385c2e/releasefile
+// XL Release version:   8.2.0-SNAPSHOT
+// Date created:         Thu Aug 30 14:41:38 CEST 2018
+
+xlr {
+  template('T1') {
+    folder('TEstnow')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-30T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('T1') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
