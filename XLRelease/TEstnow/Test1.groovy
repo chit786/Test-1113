2b28ba9b7e3d1a7b308f542dbb27ab03656dbe9d
chit786 <chit786@gmail.com>
2018-08-30 14:59:39 +0200

    v3

--- XLRelease/TEstnow/Test1.groovy
+++ XLRelease/TEstnow/Test1.groovy
@@ -1,6 +1,6 @@
 // Exported from:        https://Chitrangs-MBP.xebialabs.com:5516/xlr/#/templates/Folder21d097475e154858aef8d346c8736245-Releaseab784a32201f4149a88180f2086c2269/releasefile
 // XL Release version:   8.2.0-SNAPSHOT
-// Date created:         Thu Aug 30 14:36:32 CEST 2018
+// Date created:         Thu Aug 30 14:59:38 CEST 2018
 
 xlr {
   template('Test1') {
@@ -18,6 +18,14 @@ xlr {
                 date Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-30T14:36:21+0200')
               }
             }
+          }
+        }
+      }
+      phase('New Phase') {
+        color '#999999'
+        tasks {
+          manual('T1') {
+            
           }
         }
       }
