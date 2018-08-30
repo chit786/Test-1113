cfe49ab3cf34305c8b93369b4fdde6685b8fef0d
chit786 <chit786@gmail.com>
2018-08-30 14:11:53 +0200

    v1

---
+++ XLRelease/TEstnow/Test1.groovy
@@ -0,0 +1,16 @@
+// Exported from:        https://Chitrangs-MacBook-Pro.local:5516/xlr/#/templates/Folder21d097475e154858aef8d346c8736245-Releaseab784a32201f4149a88180f2086c2269/releasefile
+// XL Release version:   8.2.0-SNAPSHOT
+// Date created:         Thu Aug 30 14:11:52 CEST 2018
+
+xlr {
+  template('Test1') {
+    folder('TEstnow')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-30T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
