9f9bb006b01db3683546219fa504878ab7ffdb4e
chit786 <chit786@gmail.com>
2018-08-30 14:36:34 +0200

    v2

--- XLRelease/TEstnow/Test1.groovy
+++ XLRelease/TEstnow/Test1.groovy
@@ -1,6 +1,6 @@
-// Exported from:        https://Chitrangs-MacBook-Pro.local:5516/xlr/#/templates/Folder21d097475e154858aef8d346c8736245-Releaseab784a32201f4149a88180f2086c2269/releasefile
+// Exported from:        https://Chitrangs-MBP.xebialabs.com:5516/xlr/#/templates/Folder21d097475e154858aef8d346c8736245-Releaseab784a32201f4149a88180f2086c2269/releasefile
 // XL Release version:   8.2.0-SNAPSHOT
-// Date created:         Thu Aug 30 14:11:52 CEST 2018
+// Date created:         Thu Aug 30 14:36:32 CEST 2018
 
 xlr {
   template('Test1') {
@@ -8,7 +8,32 @@ xlr {
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-30T09:00:00+0200')
     phases {
       phase('New Phase') {
-        
+        color '#999999'
+        tasks {
+          manual('T1') {
+            comments {
+              comment {
+                text 'ckmmen1'
+                author 'admin'
+                date Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-30T14:36:21+0200')
+              }
+            }
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          manual('T1') {
+            
+          }
+        }
+      }
+      phase('New Phase') {
+        tasks {
+          manual('T1') {
+            
+          }
+        }
       }
     }
     
